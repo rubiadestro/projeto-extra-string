491c15819b4aece182b0d6ef0cbf95823c0c331a
rubiadestro <bia.destro@gmail.com>
2019-05-14 13:55:59 -0300

    Iniciando um Projeto

---
+++ src/main/java/br/com/rubia/projeto/ProjetoExtraSpring.java
@@ -0,0 +1,13 @@
+package br.com.rubia.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProjetoExtraSpring {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoExtraSpring.class, args);
+	}
+}
